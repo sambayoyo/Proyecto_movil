791e9024c1b69cc1372f7afec4527d27c80709a3
Samuel Rodriguez <csamuert@gmail.com>
2024-04-15 15:57:08 -0500

    prueba

--- lib/Widgets/MyHomePage.dart
+++ lib/Widgets/MyHomePage.dart
@@ -9,10 +9,31 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Stateful Widget Example'),
+      body: Center(
+        child: Text("Hello World!"),
       ),
-      body: Center(),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        currentIndex: 0,
+        onTap: (value) {
+          setState(() {});
+        },
+        elevation: 0,
+        items: [
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.two_wheeler),
+            activeIcon: const Icon(Icons.motorcycle),
+            label: 'Motors',
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.person_3_outlined),
+            activeIcon: const Icon(Icons.person_3),
+            label: 'Users',
+            backgroundColor: Colors.blue,
+          ),
+        ],
+      ),
     );
   }
 }
