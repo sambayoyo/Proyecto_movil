2806cb06e18cb6f64a46f34c30b2431f67f5adbb
Samuel Rodriguez <csamuert@gmail.com>
2024-04-15 21:01:44 -0500

    push para corregir errores

--- lib/Widgets/MyHomePage.dart
+++ lib/Widgets/MyHomePage.dart
@@ -19,99 +19,97 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.red.shade400,
-          title: !isSearching
-              ? Text("")
-              : TextField(
-                  style: GoogleFonts.roboto(
-                    fontSize: 12.0,
-                    textStyle: TextStyle(color: Colors.white),
-                  ),
-                  decoration: InputDecoration(
-                      hintText: "Search",
-                      hintStyle: TextStyle(color: Colors.white))),
-          actions: <Widget>[
-            isSearching
-                ? IconButton(
-                    icon: Icon(
-                      Icons.cancel_sharp,
-                      size: 37.0,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        this.isSearching = !this.isSearching;
-                      });
-                    },
-                  )
-                : IconButton(
-                    icon: Icon(
-                      Icons.search,
-                      size: 37.0,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        this.isSearching = !this.isSearching;
-                      });
-                    },
-                  )
-          ],
-        ),
-        body: Column(
-          children: [
-            Container(
-              height: 280.0,
-              decoration: BoxDecoration(
-                color: Colors.red.shade400,
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(250.0),
-                  bottomRight: Radius.circular(250.0),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.red.shade400,
+        title: !isSearching
+            ? Text("")
+            : TextField(
+                style: GoogleFonts.roboto(
+                  fontSize: 12.0,
+                  textStyle: TextStyle(color: Colors.white),
                 ),
-              ),
-              child: Center(
-                child: Text(
-                  'May Day',
-                  style: GoogleFonts.oleoScript(
-                    fontSize: 48,
-                    fontWeight: FontWeight.w700,
-                    textStyle: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                    hintText: "Search",
+                    hintStyle: TextStyle(color: Colors.white))),
+        actions: <Widget>[
+          isSearching
+              ? IconButton(
+                  icon: Icon(
+                    Icons.cancel_sharp,
+                    size: 37.0,
+                    color: Colors.white,
                   ),
+                  onPressed: () {
+                    setState(() {
+                      isSearching = !isSearching;
+                    });
+                  },
+                )
+              : IconButton(
+                  icon: Icon(
+                    Icons.search,
+                    size: 37.0,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      this.isSearching = !this.isSearching;
+                    });
+                  },
+                )
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 280.0,
+            decoration: BoxDecoration(
+              color: Colors.red.shade400,
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(250.0),
+                bottomRight: Radius.circular(250.0),
+              ),
+            ),
+            child: Center(
+              child: Text(
+                'May Day',
+                style: GoogleFonts.oleoScript(
+                  fontSize: 48,
+                  fontWeight: FontWeight.w700,
+                  textStyle: TextStyle(color: Colors.white),
                 ),
               ),
             ),
-            Column(
-              children: [
-                SizedBox(
-                  height: 400.0,
-                  width: 300.0,
-                  child: PageView(
-                    controller: _controller,
-                    children: const [
-                      Page1(),
-                      Page2(),
-                      Page3(),
-                    ],
-                  ),
+          ),
+          Column(
+            children: [
+              SizedBox(
+                height: 400.0,
+                width: 300.0,
+                child: PageView(
+                  controller: _controller,
+                  children: const [
+                    Page1(),
+                    Page2(),
+                    Page3(),
+                  ],
                 ),
-              ],
-            )
-          ],
-        ),
-        bottomNavigationBar: CurvedNavigationBar(
-          backgroundColor: Colors.white,
-          color: Colors.grey.shade800,
-          onTap: (index) {},
-          items: [
-            Icon(Icons.wallet, color: Colors.white),
-            Icon(Icons.add, color: Colors.white),
-            Icon(Icons.settings, color: Colors.white),
-          ],
-        ),
+              ),
+            ],
+          )
+        ],
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.white,
+        color: Colors.grey.shade800,
+        onTap: (index) {},
+        items: [
+          Icon(Icons.wallet, color: Colors.white),
+          Icon(Icons.add, color: Colors.white),
+          Icon(Icons.settings, color: Colors.white),
+        ],
       ),
     );
   }
