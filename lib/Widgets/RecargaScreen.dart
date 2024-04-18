8405f7e6e4bb7cfbdf510da0b02c7e3649e11614
unknown <ahora608@gmail.com>
2024-04-18 16:16:11 -0500

    actu de recarga

--- lib/Widgets/RecargaScreen.dart
+++ lib/Widgets/RecargaScreen.dart
@@ -5,11 +5,8 @@ class RecargaScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Recargar'),
-      ),
       body: Container(
-        color: Color.fromARGB(255, 178, 209, 216), // Fondo azul claro
+        color: Color(0xFF101010), // Color principal
         child: Padding(
           padding: EdgeInsets.all(20.0),
           child: Column(
@@ -17,30 +14,38 @@ class RecargaScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                'Recargue de forma segura y sencilla  ',
-                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                'Seleccione el método de recarga que prefiera:',
-                style: TextStyle(fontSize: 18.0),
+                'Recargue de forma segura y sencilla',
+                style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
                 textAlign: TextAlign.center,
               ),
               SizedBox(height: 40.0),
+              Text(
+                'Seleccione el método de recarga que prefiera:',
+                style: TextStyle(fontSize: 20.0, color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 60.0),
               ElevatedButton(
                 onPressed: () {
                   _mostrarDialogoRecargaTarjeta(context);
                 },
                 style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0), backgroundColor: Color(0xFFEF315D), // Color de botón
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
                 ),
-                child: Column(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.credit_card, size: 40.0),
-                    SizedBox(height: 10.0),
+                    Icon(Icons.credit_card, size: 40.0, color: Colors.white),
+                    SizedBox(width: 10.0),
                     Text('Recargar con tarjeta',
-                        style: TextStyle(fontSize: 18.0)),
+                        style: TextStyle(fontSize: 20.0, color: Colors.white)),
                   ],
                 ),
               ),
@@ -50,14 +55,19 @@ class RecargaScreen extends StatelessWidget {
                   _generarNumeroAleatorio(context);
                 },
                 style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0), backgroundColor: const Color(0xFFEF315D), // Color de botón
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
                 ),
-                child: Column(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.money, size: 40.0),
-                    SizedBox(height: 10.0),
+                    Icon(Icons.money, size: 40.0, color: Colors.white),
+                    SizedBox(width: 10.0),
                     Text('Recargar en efectivo',
-                        style: TextStyle(fontSize: 18.0)),
+                        style: TextStyle(fontSize: 20.0, color: Colors.white)),
                   ],
                 ),
               ),
